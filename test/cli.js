609cb45a637d559a9c6733cc33450b8bc91c9081
maxlath <k@maxlath.eu>
2017-02-19 15:23:52 +0100

    added cli tests

---
+++ test/cli.js
@@ -0,0 +1,14 @@
+require('should')
+const execa = require('execa')
+
+describe('cli', () => {
+  describe('piped', () => {
+    it('should accept a file path as first argument', (done) => {
+      execa.shell('cat package.json | jd license')
+      .then(res => {
+        res.stdout.should.equal('MIT')
+        done()
+      })
+    })
+  })
+})
