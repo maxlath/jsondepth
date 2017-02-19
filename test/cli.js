a259024cd79d137ef74671ae8115ebcffafb4675
maxlath <k@maxlath.eu>
2017-02-19 17:33:33 +0100

    standalone mode: accept a file path as first argument

--- test/cli.js
+++ test/cli.js
@@ -3,7 +3,7 @@ const execa = require('execa')
 
 describe('cli', () => {
   describe('piped', () => {
-    it('should accept a file path as first argument', (done) => {
+    it('should accept JSON on stdin', (done) => {
       execa.shell('cat package.json | jd license')
       .then(res => {
         res.stdout.should.equal('MIT')
@@ -18,4 +18,16 @@ describe('cli', () => {
       })
     })
   })
+  // /!\ Can't be tested at the moment as it would run it in a child process
+  // which is the one big non-solved case
+  // describe('standalone', () => {
+  //   it('should accept a file path as first argument', (done) => {
+  //     execa.shell('jd package.json license')
+  //     .then(res => {
+  //       console.log('res', res)
+  //       res.stdout.should.equal('MIT')
+  //       done()
+  //     })
+  //   })
+  // })
 })
