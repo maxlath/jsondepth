65668219b25936ff35d4e96c47bb15c7fc404886
maxlath <k@maxlath.eu>
2019-08-12 15:20:46 +0200

    test: cleanup

--- test/cli.js
+++ test/cli.js
@@ -3,31 +3,35 @@ const execa = require('execa')
 
 describe('cli', () => {
   describe('piped', () => {
-    it('should accept JSON on stdin', (done) => {
+    it('should accept JSON on stdin', done => {
       execa.shell('cat package.json | jd license')
       .then(res => {
         res.stdout.should.equal('MIT')
         done()
       })
+      .catch(done)
     })
-    it('should not throw when the output is truncated', (done) => {
+
+    it('should not throw when the output is truncated', done => {
       execa.shell('cat package.json | jd scripts | head -n 1')
       .then(res => {
         res.code.should.equal(0)
         done()
       })
+      .catch(done)
     })
   })
   // /!\ Can't be tested at the moment as it would run it in a child process
   // which is the one big non-solved case
   // describe('standalone', () => {
-  //   it('should accept a file path as first argument', (done) => {
+  //   it('should accept a file path as first argument', done => {
   //     execa.shell('jd package.json license')
   //     .then(res => {
   //       console.log('res', res)
   //       res.stdout.should.equal('MIT')
   //       done()
   //     })
+  //     .catch(done)
   //   })
   // })
 })
